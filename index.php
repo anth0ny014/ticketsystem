41fcda9515bfd5ebe2b959e0c0125874db59d859
anth0ny014 <velasco.markanthony015@gmail.com>
2021-03-08 19:09:31 +0800

    commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
+    <script src="javascripts/jquery.min.js"></script>
+    <script src="javascripts/popper.min.js"></script>
+    <script src="javascripts/bootstrap.min.js"></script>
+    <title>Ticket System</title>
+</head>
+<body>
+    
+</body>
+</html>
